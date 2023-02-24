44b703ca530ce7e55a8356e9566a58375ecd9b3d
unknown <claydson21@hotmail.com>
2023-02-23 21:56:32 -0300

    feat:Refatorando codigo para salar todas as evidencias no banco de dados.

--- 7.Queue.ProcessEvidences/src/jobs/CalculeAndUpdateCloseJobSugestion/index.ts
+++ 7.Queue.ProcessEvidences/src/jobs/CalculeAndUpdateCloseJobSugestion/index.ts
@@ -5,7 +5,7 @@ import ClosedResult from '../../Interfaces/ClosedResult';
 import ClosedJobEvidence from '../../Interfaces/ClosedJobEvidence';
 
 export default async function (channel: amqp.Channel) {
-    // TODO : No momento, ProcessEvidence está pegando apenas um resultado da api para teste
+    // TODO : No momento, ProcessEvidence está pegando apenas um resultado da api para teste*
     // O serviço generic worker ainda precisa ser adaptado para enviar chunks que possam se identificar como
     // ultimo ou não, para que o processamento aqui possa ser específico para cada vaga
 
@@ -14,20 +14,26 @@ export default async function (channel: amqp.Channel) {
     });
 
     const res: any = await axios.get('http://localhost:5226/api/ClosedResult');
-    console.log(res.data)
+    // console.log(res.data)
+    console.log('Comeca daqui')
     const jobs: ClosedResult[] = res.data;
-    const jobId = jobs[0].jobId;
-    const allJobsWithThisJobId = jobs.filter(j => j.jobId == jobId)
-    const countIsClosed = allJobsWithThisJobId.filter(j => j.value == '1').length
+    
+    for (const key in jobs) {
+        const jobId = jobs[key].jobId;
+        const allJobsWithThisJobId = jobs.filter(j => j.jobId == jobId)
+        const countIsClosed = allJobsWithThisJobId.filter(j => j.value == '1').length
+    
+        const evidence: ClosedJobEvidence = {
+            JobId: jobId,
+            ActualStatus: 0,
+            CountIsClosed: countIsClosed,
+            PossiblyClosed: countIsClosed >= 1
+        }
 
-    const evidence: ClosedJobEvidence = {
-        JobId: jobId,
-        ActualStatus: 0,
-        CountIsClosed: countIsClosed,
-        PossiblyClosed: countIsClosed >= 1
+        console.log(evidence)
+
+        channel.sendToQueue('Queue.Data4base.Processed', Buffer.from(
+            JSON.stringify(evidence)
+        ))
     }
-
-    channel.sendToQueue('Queue.Database.Processed', Buffer.from(
-        JSON.stringify(evidence)
-    ))
 }
